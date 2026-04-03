f3e168b1f0573f15bbef4bb54422e304ab539dac
Michael Noukhovitch <michaeln@allenai.org>
2026-04-03 21:37:21 +0000

    200 steps

--- examples/dppo_trainer/run_qwen30b_dppo.sh
+++ examples/dppo_trainer/run_qwen30b_dppo.sh
@@ -307,10 +307,10 @@ REWARD_CONFIG="
     trainer.nnodes=$NNODES \
     trainer.val_before_train=False \
     trainer.log_val_generations=100 \
-    trainer.save_freq=-1 \
-    trainer.test_freq=10 \
+    trainer.save_freq=25 \
+    trainer.test_freq=25 \
     trainer.total_epochs=10 \
-    trainer.total_training_steps=500 \
+    trainer.total_training_steps=200 \
     $ACTOR_CONFIG \
     $CIRITC_CONFIG \
     $ROLLOUT_CONFIG \
