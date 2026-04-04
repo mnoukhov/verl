0bffd5f74643c17a6920109b0088074b6395196e
Michael Noukhovitch <michaeln@allenai.org>
2026-04-04 20:18:07 +0000

    guessing clip

--- examples/dppo_trainer/run_qwen30b_dppo.sh
+++ examples/dppo_trainer/run_qwen30b_dppo.sh
@@ -39,7 +39,7 @@ elif [[ $LOSS_MODE == "vanilla" ]]; then
     clip_ratio_high=${CLIP_HIGH:-0.28}
 elif [[ $LOSS_MODE == "tvpo" ]]; then
     # GRPO baseline
-    clip_ratio=0.15
+    clip_ratio=0.005
     clip_ratio_low=${CLIP_LOW:-0.15}
     clip_ratio_high=${CLIP_HIGH:-0.15}
 else
