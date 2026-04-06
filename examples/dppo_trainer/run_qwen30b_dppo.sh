f05dfef83236b0242f541a3e1ce136d55689db25
Michael Noukhovitch <michaeln@allenai.org>
2026-04-06 19:34:16 +0000

    set clip with clip high

--- examples/dppo_trainer/run_qwen30b_dppo.sh
+++ examples/dppo_trainer/run_qwen30b_dppo.sh
@@ -39,9 +39,9 @@ elif [[ $LOSS_MODE == "vanilla" ]]; then
     clip_ratio_high=${CLIP_HIGH:-0.28}
 elif [[ $LOSS_MODE == "tvpo" ]]; then
     # GRPO baseline
-    clip_ratio=0.005
+    clip_ratio=${CLIP_HIGH:-0.005}
     clip_ratio_low=${CLIP_LOW:-0.15}
-    clip_ratio_high=${CLIP_HIGH:-0.15}
+    clip_ratio_high=${CLIP_HIGH:-0.005}
 else
     echo "Invalid loss mode: $LOSS_MODE"
     exit 1
